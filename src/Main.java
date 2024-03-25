a4ef6fdf145c6e42726fbb64365833b387c2774a
zincc92 <clement.pompei@gmail.com>
2024-03-25 17:22:34 +0100

    ajout panneau de connexion avec le main pour ouvrir

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import VUE.connexion;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Informations de connexion à la base de données
+        String url = "jdbc:mysql://localhost:3306/projetcinema";
+        String utilisateur = "root"; // Remplacez par votre nom d'utilisateur MySQL
+        String motDePasse = ""; // Remplacez par votre mot de passe MySQL
+
+        // Connexion à la base de données
+        try {
+            Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+            System.out.println("Connexion à la base de données réussie !");
+            // Vous pouvez maintenant exécuter vos requêtes SQL à partir de la connexion
+            // N'oubliez pas de fermer la connexion lorsque vous avez terminé :
+            // connexion.close();
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
+        }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                System.out.println("Nouvelle instance");
+                new connexion();
+            }
+        });
     }
 }
 
