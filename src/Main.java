85c28d54f627c1f97fcff9347232a3dedd28cd53
zincc92 <clement.pompei@gmail.com>
2024-03-26 12:49:00 +0100

    ajout accueil java commencé

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ public class Main {
                 System.out.println("Nouvelle instance");
                 new connexion();
                 new inscription();
+                new accueil();
             }
         });
     }
