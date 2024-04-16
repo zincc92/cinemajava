4e40e6def08adb8337083c87103a0094aa75b94f
NicolasLMP <nicolas.lambropoulos54@gmail.com>
2024-04-16 16:08:28 +0200

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import VUE.*;
+import MODELE.*;
+import CONTROLLEUR.*;
+
+import VUE.connexion;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Informations de connexion à la base de données
+        String url = "jdbc:mysql://localhost:3306/cinemaprojet";
+        String utilisateur = "root"; // Remplacez par votre nom d'utilisateur MySQL
+        String motDePasse = ""; // Remplacez par votre mot de passe MySQL
+
+        // Connexion à la base de données
+        try {
+            Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+            System.out.println("Connexion à la base de données réussie !");
+            // Vous pouvez maintenant exécuter vos requêtes SQL à partir de la connexion
+            // N'oubliez pas de fermer la connexion lorsque vous avez terminé :
+            // connexion.close();
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    //Création de la fenêtre d'utilisation de l'application
+                    utilisateurControlleur utilisateurControlleur = new utilisateurControlleur(connexion);
+                    JFrame frame = new JFrame("CinemaAPP");
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setSize(1366, 768);
+                    frame.setResizable(false);
+
+                    barreDeTache menuBar = new barreDeTache(frame, utilisateurControlleur, connexion);
+                    frame.setJMenuBar(menuBar);
+
+                    //Accueil accueil = new Accueil();
+                    //frame.add(accueil);
+
+                    frame.setVisible(true);
+                }
+            });
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
+        }
+
     }
 }
+
